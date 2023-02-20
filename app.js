4ea0aa20d12b49d029f551b8a2c7862d8d7d7bdf
JisuPark <sciencho@naver.com>
2023-02-20 16:07:42 +0900

    upload favicon

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var favicon = require('serve-favicon');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
